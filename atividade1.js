c47b83a662bd44835de76a050f6adfbdb611dfc8
Max <164526851+MaadMa4x@users.noreply.github.com>
2025-05-16 11:57:30 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,4 @@
+const jsonString = '{"nome":"Nana","idade":17, "cidade":"Curitiba"}';
+const objetoJS = JSON.parse(jsonString);
+
+console.log(objetoJS); // Nana
---
+++ atividade2.js
@@ -0,0 +1,9 @@
+const livro = {
+    titulo: "É assim que acaba",
+    autor: "Colleen Hoover",
+    ano: 2016
+};
+
+const jsonGerado = JSON.stringify(livro);
+
+console.log(jsonGerado);
